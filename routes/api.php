5b53f1e315a272e033e465a3116d591c23643b68
bajarigergo <gergob10@gmail.com>
2024-11-19 09:56:18 +0100

    api útvonalak

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("/products", [ProductController::class, 'index']);
+Route::get("/product/{product_id}", [ProductController::class, 'show']);
+Route::post("/product/{product_id}", [ProductController::class, 'store']);
+Route::put("/product/{product_id}", [ProductController::class, 'update']);
+Route::delete("/product/{product_id}", [ProductController::class, 'destroy']);
